c4d99275bb2e5a93dd0a84db6bfa9676684a1a10
lolacocs <lolacossss@gmail.com>
2025-05-14 03:40:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Day1 from "./Day1"; // импорт твоего компонента
+import App from "./App"; // Импорт главного экрана
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Day1 />
+    <App />
   </React.StrictMode>
 );
