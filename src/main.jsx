fbbdd9e7da386fe05f1f5652477ff1deb451b2b6
lolacocs <lolacossss@gmail.com>
2025-05-14 04:02:12 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Импорт главного экрана
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App";        // Главный экран
+import Day1 from "./Day1";      // День 1
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/day1" element={<Day1 />} />
+        {/* можно добавить еще:
+        <Route path="/day2" element={<Day2 />} />
+        <Route path="/day3" element={<Day3 />} />
+        */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
