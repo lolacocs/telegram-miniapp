00c609177185fabee924351f81f73c751efb76d3
lolacocs <lolacossss@gmail.com>
2025-05-14 03:25:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Day1 from "./Day1";
+import Day1 from "./Day1.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const path = window.location.pathname;
