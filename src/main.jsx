6b5cc1e1d2fd543a1c223965a62b699c4616bd09
lolacocs <lolacossss@gmail.com>
2025-05-14 03:32:03 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Day1 from "./Day1";
+import Day1 from "./Day1"; // импорт твоего компонента
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const path = window.location.pathname;
-
-if (path === "/day1.html") {
-  root.render(<Day1 />);
-} else {
-  root.render(<App />);
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Day1 />
+  </React.StrictMode>
+);
